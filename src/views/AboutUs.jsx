f86d5c10573890322b08450e9f7d35140abb89d8
srikar1980 <46640210+srikar1980@users.noreply.github.com>
2024-06-20 14:50:09 +0530

    aboutus page is done

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,13 +1,31 @@
+import aboutUsImage from '../assets/aboutus-img.png';
+
 const AboutUs = () => {
   return (
-    <section id="about-us" className="section about-us">
-      <div className="content">
-        <h2>About Us</h2>
-        <p>
-          We are a company dedicated to delivering top-notch services and
-          unparalleled customer satisfaction. Learn more about our journey and
-          values.
-        </p>
+    <section className="intro">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-5 intro-image">
+            <img src={aboutUsImage} alt="Aboutus Image" className="img-fluid" />
+          </div>
+          <div className="col-lg-7 intro-content ">
+            <p>Capital Bridge:</p>
+            <h1>About Us</h1>
+            <p>
+              At Capital Bridge, we believe that the cornerstone of success lies
+              in continuous professional development. Our mission is to bridge
+              the gap between potential and performance by offering cutting-edge
+              training solutions tailored for professionals, corporations, and
+              sales teams. With a focus on innovation, expertise, and results,
+              we are committed to transforming your workforce into a dynamic and
+              highly skilled team ready to meet the challenges of todays
+              competitive market.
+            </p>
+            <div className="btn-group">
+              <button className="btn intro-btn1">Get Quote</button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
