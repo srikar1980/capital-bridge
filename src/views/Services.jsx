ae4c22e0edfef3fca2b357161e160eddd859fadb
srikar1980 <46640210+srikar1980@users.noreply.github.com>
2024-06-20 16:12:19 +0530

    services section is done.

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,13 +1,28 @@
+import '../css/Services.css';
+import { servicescards } from '../data/servicesdata';
+
 const Services = () => {
   return (
-    <section id="services" className="section services">
-      <div className="content">
-        <h2>Our Services</h2>
-        <p>
-          We offer a wide range of services to meet all your needs. From
-          consultation to execution, we ensure quality and excellence at every
-          step.
-        </p>
+    <section className="section services">
+      <div className="row align-items-center services-content">
+        <h1>Services</h1>
+        <div className="col-lg-12 ">
+          <div className="service-cards">
+            {servicescards.map((service, index) => (
+              <div className="service-card" key={index}>
+                <div className="service-image">
+                  {service.image && (
+                    <img src={service.image} alt={service.title} />
+                  )}
+                </div>
+                <div className="service-details">
+                  <h3>{service.title}</h3>
+                  <p>{service.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
