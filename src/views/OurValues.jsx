6cb3df77bfccbcee95c6d161e258f10037a31a4c
srikar1980 <46640210+srikar1980@users.noreply.github.com>
2024-06-20 16:42:17 +0530

    first four sections are done.

--- src/views/OurValues.jsx
+++ src/views/OurValues.jsx
@@ -1,12 +1,47 @@
+import '../css/Ourvalues.css';
+import aboutUsVideo from '../assets/short-video.mp4'; // Import your video file
+
 const OurValues = () => {
   return (
     <section className="section our-values">
-      <div className="content">
-        <h2>Our Values</h2>
-        <p>
-          Integrity, innovation, and customer satisfaction are at the core of
-          our values. Discover what drives us to be the best in the industry.
-        </p>
+      <div className="container">
+        <div className="row align-items-center">
+          <h2 className="ourvalues-title">
+            Your Gateway to Professional Excellence
+          </h2>
+          <p className="ourvalues-desc">
+            Unleashing Potential with Innovative Training Solutions for
+            Professionals and Corporations
+          </p>
+          <div className="col-lg-6 intro-image">
+            <video controls autoPlay loop muted className="video-fluid">
+              <source src={aboutUsVideo} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="col-lg-6 intro-content">
+            <div>
+              <h4>Mission</h4>
+              <p>
+                Our mission at Capital Bridge is to empower individuals and
+                organizations by providing superior training and development
+                solutions that foster growth, efficiency, and success. We aim to
+                be the trusted partner in your journey towards professional
+                excellence.
+              </p>
+            </div>
+            <div>
+              <h4>Vision</h4>
+              <p>
+                Our vision is to become the leading provider of professional
+                training services globally, recognized for our commitment to
+                quality, innovation, and measurable results. We strive to create
+                a world where continuous learning and development are integral
+                to achieving personal and organizational goals.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
